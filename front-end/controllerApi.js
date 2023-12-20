49d896e4e68f4d8d466d45aeb225cefecd6f0a6b
vanderPato <vander108@hotmail.com>
2023-12-20 00:38:55 -0300

    Atualização no codigo

--- front-end/controllerApi.js
+++ front-end/controllerApi.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
     document.addEventListener("DOMContentLoaded", function() {
         const frenteDown = document.getElementById("frenteDown");
@@ -14,12 +13,12 @@
 
         frenteDown.addEventListener("mousedown", function() {
             frentePrecionado = true;
-            atualizarDownFrente(frentePrecionado, trazPrecionado, esquerdaPrecionado, direitaPrecionado);
+            atualizarDown(frentePrecionado, trazPrecionado, esquerdaPrecionado, direitaPrecionado);
         });
 
         frenteDown.addEventListener("mouseup", function() {
             frentePrecionado = false;
-            atualizarDownFrente(frentePrecionado, trazPrecionado, esquerdaPrecionado, direitaPrecionado);
+            atualizarDown(frentePrecionado, trazPrecionado, esquerdaPrecionado, direitaPrecionado);
         });
 
 
@@ -27,24 +26,24 @@
 
         trazDown.addEventListener("mousedown", function() {
             trazPrecionado = true;
-            atualizarDownFrente(frentePrecionado, trazPrecionado, esquerdaPrecionado, direitaPrecionado);
+            atualizarDown(frentePrecionado, trazPrecionado, esquerdaPrecionado, direitaPrecionado);
         });
 
         trazDown.addEventListener("mouseup", function() {
             trazPrecionado = false;
-            atualizarDownFrente(frentePrecionado, trazPrecionado, esquerdaPrecionado, direitaPrecionado);
+            atualizarDown(frentePrecionado, trazPrecionado, esquerdaPrecionado, direitaPrecionado);
         });
 
 
 
         esquerdaDown.addEventListener("mousedown", function() {
             esquerdaPrecionado = true;
-            atualizarDownFrente(frentePrecionado, trazPrecionado, esquerdaPrecionado, direitaPrecionado);
+            atualizarDown(frentePrecionado, trazPrecionado, esquerdaPrecionado, direitaPrecionado);
         });
 
         esquerdaDown.addEventListener("mouseup", function() {
             esquerdaPrecionado = false;
-            atualizarDownFrente(frentePrecionado, trazPrecionado, esquerdaPrecionado, direitaPrecionado);
+            atualizarDown(frentePrecionado, trazPrecionado, esquerdaPrecionado, direitaPrecionado);
         });
 
 
@@ -53,12 +52,12 @@
 
         direitaDown.addEventListener("mousedown", function() {
             direitaPrecionado = true;
-            atualizarDownFrente(frentePrecionado, trazPrecionado, esquerdaPrecionado, direitaPrecionado);
+            atualizarDown(frentePrecionado, trazPrecionado, esquerdaPrecionado, direitaPrecionado);
         });
 
         direitaDown.addEventListener("mouseup", function() {
             direitaPrecionado = false;
-            atualizarDownFrente(frentePrecionado, trazPrecionado, esquerdaPrecionado, direitaPrecionado);
+            atualizarDown(frentePrecionado, trazPrecionado, esquerdaPrecionado, direitaPrecionado);
         });
 
 
@@ -70,7 +69,7 @@
 
 
 
-async function atualizarDownFrente(valorFrente, valorTraz, valorEsquerdo, valorDireito) {
+async function atualizarDown(valorFrente, valorTraz, valorEsquerdo, valorDireito) {
     const url = 'http://192.168.1.26:3000/api/data';
     const valorDireitaSpan = document.getElementById('direita');
     const valorEsquerdaSpan = document.getElementById('esquerda');
@@ -95,71 +94,12 @@ async function atualizarDownFrente(valorFrente, valorTraz, valorEsquerdo, valorD
         valorEsquerdaSpan.innerText = response.data.esquerda.toString();
         valorDireitaSpan.innerText = response.data.direita.toString();
 
-=======
-    
-    async function atualizarFrente() {
-        const url = 'http://192.168.1.24:3000/api/data';
-        const valorDireitaSpan = document.getElementById('direita');
-        const valorEsquerdaSpan = document.getElementById('esquerda');
-        const valoTrazSpan = document.getElementById('traz');
-        const valorFrenteSpan = document.getElementById('frente');
-        const valorFrente = valorFrenteSpan.innerText === 'true';
-        
-        
-        const newData = {
-            id: 0,
-            frente: !valorFrente,
-            traz:false,
-            esquerda:false,
-            direita:false
-        };
-        try{
-            const response = await axios.patch(url, newData);
-            
-            valorFrenteSpan.innerText = response.data.frente.toString();
-            valoTrazSpan.innerText = response.data.traz.toString();
-            valorEsquerdaSpan.innerText = response.data.esquerda.toString();
-            valorDireitaSpan.innerText = response.data.direita.toString();
- 
-        
-        } catch(error) {
-            console.log('Erro ao fazer a solicitação PATCH:', error);
-    }
-}
-
-
-async function atualizarTraz() {
-    const url = 'http://192.168.1.24:3000/api/data';
-    const valorDireitaSpan = document.getElementById('direita');
-    const valorFrenteSpan = document.getElementById('frente');
-    const valorEsquerdaSpan = document.getElementById('esquerda');
-    const valoTrazSpan = document.getElementById('traz');
-    const valorTraz = valoTrazSpan.innerText === 'true';
-
-    const newData = {
-        id: 0,
-        frente:false,
-        traz:!valorTraz,
-        esquerda:false,
-        direita:false
-    };
-    try{
-        const response = await axios.patch(url, newData);
-        
-        valoTrazSpan.innerText = response.data.traz.toString();
-        valorFrenteSpan.innerText = response.data.frente.toString();
-        valorEsquerdaSpan.innerText = response.data.esquerda.toString();
-        valorDireitaSpan.innerText = response.data.direita.toString();
-
-
->>>>>>> 3f86025c9505350accf17822f166d04f0ed5f131
     
     } catch(error) {
         console.log('Erro ao fazer a solicitação PATCH:', error);
 }
 }
 
-<<<<<<< HEAD
 
 document.addEventListener("DOMContentLoaded",  function( ) {
    const valorDireitaSpan = document.getElementById('direitaCar');
@@ -178,7 +118,7 @@ document.addEventListener("DOMContentLoaded",  function( ) {
         traz = false;
         direita = false;
         esquerda = false;
-        atualizarFrenteCar(frente, traz , esquerda, direita);
+        atualizarValorCar(frente, traz , esquerda, direita);
 
     });
    
@@ -188,7 +128,7 @@ document.addEventListener("DOMContentLoaded",  function( ) {
         traz = !traz;
         direita = false;
         esquerda = false;
-        atualizarFrenteCar(frente, traz , esquerda, direita);
+        atualizarValorCar(frente, traz , esquerda, direita);
 
     });
 
@@ -199,7 +139,7 @@ document.addEventListener("DOMContentLoaded",  function( ) {
         traz =false;
         esquerda  = !esquerda;
         direita = false;
-        atualizarFrenteCar(frente, traz , esquerda, direita);
+        atualizarValorCar(frente, traz , esquerda, direita);
 
     });
   
@@ -210,19 +150,19 @@ document.addEventListener("DOMContentLoaded",  function( ) {
         esquerda  = false;
         direita = !direita;
 
-        atualizarFrenteCar(frente, traz , esquerda, direita);
+        atualizarValorCar(frente, traz , esquerda, direita);
     });
   
 
 })
 
-async function atualizarFrenteCar(frente, traz, esquerda, direita) {
+async function atualizarValorCar(frente, traz, esquerda, direita) {
+    const url = 'http://192.168.1.26:3000/api/data';
    const valorDireitaSpan = document.getElementById('direita');
    const valorEsquerdaSpan = document.getElementById('esquerda');
    const valoTrazSpan = document.getElementById('traz');
    const valorFrenteSpan = document.getElementById('frente');
 
-    const url = 'http://192.168.1.26:3000/api/data';
 
     const newData = {
         id: 0,
@@ -239,70 +179,6 @@ async function atualizarFrenteCar(frente, traz, esquerda, direita) {
         valorEsquerdaSpan.innerText = response.data.esquerda.toString();
         valorDireitaSpan.innerText = response.data.direita.toString();
 
-=======
-async function atualizarEsquerda() {
-    const url = 'http://192.168.1.24:3000/api/data';
-    const valorDireitaSpan = document.getElementById('direita');
-    const valoTrazSpan = document.getElementById('traz');
-    const valorFrenteSpan = document.getElementById('frente');
-    const valorEsquerdaSpan = document.getElementById('esquerda');
-    const valorEsquerda = valorEsquerdaSpan.innerText === 'true';
-
-
-    
-    
-    const newData = {
-        id: 0,
-        frente: false,
-        traz:false,
-        esquerda:!valorEsquerda,
-        direita:false
-    };
-    try{
-        const response = await axios.patch(url, newData);
-        
-        valorEsquerdaSpan.innerText = response.data.esquerda.toString();
-        valorFrenteSpan.innerText = response.data.frente.toString();
-        valoTrazSpan.innerText = response.data.traz.toString();
-        valorDireitaSpan.innerText = response.data.direita.toString();
-
-
-    
-    } catch(error) {
-        console.log('Erro ao fazer a solicitação PATCH:', error);
-}
-}
-
-
-
-async function atualizarDireita() {
-    const url = 'http://192.168.1.24:3000/api/data';
-    
-    const valoTrazSpan = document.getElementById('traz');
-    const valorFrenteSpan = document.getElementById('frente');
-    const valorEsquerdaSpan = document.getElementById('esquerda');
-    const valorDireitaSpan = document.getElementById('direita');
-    const valorDiteita = valorDireitaSpan.innerText === 'true';
-
-
-    
-    
-    const newData = {
-        id: 0,
-        frente: false,
-        traz:false,
-        esquerda:false,
-        direita:!valorDiteita
-    };
-    try{
-        const response = await axios.patch(url, newData);
-        
-        valorDireitaSpan.innerText = response.data.direita.toString();
-        valorEsquerdaSpan.innerText = response.data.esquerda.toString();
-        valorFrenteSpan.innerText = response.data.frente.toString();
-        valoTrazSpan.innerText = response.data.traz.toString();
-
->>>>>>> 3f86025c9505350accf17822f166d04f0ed5f131
     
     } catch(error) {
         console.log('Erro ao fazer a solicitação PATCH:', error);
